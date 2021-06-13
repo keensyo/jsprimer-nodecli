67499ea1b86149fa1ec9518d5fe278c9e38985f4
keensyo <k66mdg55@gmail.com>
2021-06-13 16:21:51 +0900

    markedライブラリを導入

--- main.js
+++ main.js
@@ -2,10 +2,21 @@
 const program = require("commander");
 // fsモジュールをfsオブジェクトとしてインポートする
 const fs = require("fs");
+// markedモジュールをmarkedオブジェクトとしてインポートする
+const marked = require("marked");
 
-// コマンドライン引数からファイルパスを取得する
+// gfmオプションを定義する
+program.option("--gfm", "GFMを有効にする");
 program.parse(process.argv);
 const filePath = program.args[0];
+
+// コマンドライン引数のオプションを取得する
+const options = program.opts();
+
+// コマンドライン引数で指定されなかったオプションにデフォルト値を上書きする
+const cliOptions = {
+    gfm: options.gfm ?? false,
+};
 
 // ファイルを非同期で読み込む
 fs.readFile(filePath,　{ encoding: "utf8" }, (err, file) => {
@@ -15,5 +26,9 @@ fs.readFile(filePath,　{ encoding: "utf8" }, (err, file) => {
         process.exit(1);
         return;
     }
-    console.log(file);
+    // MarkdownファイルをHTML文字列に変換する
+    const html = marked(file, {
+        gfm: cliOptions.gfm,
+    });
+    console.log(html);
 });
