1cc772fcc0ee4bc7a78aa018616d7a2589348c76
keensyo <k66mdg55@gmail.com>
2021-06-13 16:07:09 +0900

    ファイルを非同期で読み込む処理を追加&エラーハンドリングも追加

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-// commanderモジュールをprogramとしてインポートする
+// commanderモジュールをprogramオブジェクトとしてインポートする
 const program = require("commander");
+// fsモジュールをfsオブジェクトとしてインポートする
+const fs = require("fs");
 
-// コマンドライン引数をcommanderでパースする
+// コマンドライン引数からファイルパスを取得する
 program.parse(process.argv);
-
-// ファイルパスをprogram.args配列から取り出す
 const filePath = program.args[0];
-console.log(filePath);
+
+// ファイルを非同期で読み込む
+fs.readFile(filePath,　{ encoding: "utf8" }, (err, file) => {
+    if (err) {
+        console.error(err.message);
+        // 終了ステータス 1（一般的なエラー）としてプロセスを終了する
+        process.exit(1);
+        return;
+    }
+    console.log(file);
+});
